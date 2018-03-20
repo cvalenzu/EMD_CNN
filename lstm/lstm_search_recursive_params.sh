4ba145f9ee3f686cbef6a0a1cfad785d827a7d64
Camilo Valenzuela <cacke91@gmail.com>
2018-03-20 00:03:04 -0300

    Added normal train

---
+++ lstm/lstm_search_recursive_params.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=1
+
+
+preproc=( std minmax1 minmax2 )
+timesteps=( 24 48 72 168 336 672 )
+for timestep in ${timesteps[@]};do
+		for pre in ${preproc[@]};do
+			python lstm_selecting_recursive_params.py  --outputs 169 --timesteps $timestep --epochs 10 --preprocess $pre --verbose t ../data/canela.csv
+		done
+done
